f8c2cf20fe831af9b8c2387f7bb04d6f46e0d882
Max Samsonenko <samsonenko.maxwork@gmail.com>
2024-05-13 11:36:32 +0300

    completed mongodb hw

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,17 +1,14 @@
 import * as contactsService from "../services/contactsServices.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
-import {
-	createContactSchema,
-	updateContactSchema,
-} from "../schemas/contactsSchemas.js";
 import HttpError from "../helpers/HttpError.js";
 
-const getAllContacts = async (req, res, next) => {
+const getAllContacts = async (req, res) => {
 	const result = await contactsService.listContacts();
 	res.json(result);
 };
 
-const getOneContact = async (req, res, next) => {
+const getOneContact = async (req, res) => {
+	console.log(req.params);
 	const { id } = req.params;
 	const contact = await contactsService.getContactById(id);
 	if (!contact) {
@@ -20,30 +17,31 @@ const getOneContact = async (req, res, next) => {
 	res.json(contact);
 };
 
-const deleteContact = async (req, res, next) => {
+const deleteContact = async (req, res) => {
 	const { id } = req.params;
 	const result = await contactsService.removeContact(id);
 	if (!result) {
 		throw HttpError(404);
 	}
-	res.status(200).json(result);
+	res.status(200).json({ message: "Contact deleted" });
 };
 
-const createContact = async (req, res, next) => {
-	const { error } = createContactSchema.validate(req.body);
-	if (error) {
-		throw HttpError(400, error.message);
-	}
-	const { name, email, phone } = req.body;
-	const newContact = await contactsService.addContact(name, email, phone);
+const createContact = async (req, res) => {
+	const newContact = await contactsService.addContact(req.body);
+
 	res.status(201).json(newContact);
 };
 
-const updateContact = async (req, res, next) => {
-	const { error } = updateContactSchema.validate(req.body);
-	if (error) {
-		throw HttpError(400, error.message);
+const updateContact = async (req, res) => {
+	const { id } = req.params;
+	const result = await contactsService.updateContactById(id, req.body);
+	if (!result) {
+		throw HttpError(404);
 	}
+	res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
 	const { id } = req.params;
 	const result = await contactsService.updateContactById(id, req.body);
 	if (!result) {
@@ -58,4 +56,5 @@ export default {
 	deleteContact: ctrlWrapper(deleteContact),
 	createContact: ctrlWrapper(createContact),
 	updateContact: ctrlWrapper(updateContact),
+	updateStatusContact: ctrlWrapper(updateStatusContact),
 };
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,11 +1,19 @@
 import Contact from "../models/Contact.js";
 
-export const listContacts = async () => Contact.find();
+export const listContacts = (search = {}) => {
+	const { filters = {} } = search;
+	return Contact.find(filters);
+};
 
-export const getContactById = async (contactId) => {};
+export const getContactById = async (contactId) => {
+	const result = await Contact.findById(contactId);
+	return result;
+};
 
-export const removeContact = async (contactId) => {};
+export const removeContact = (contactId) =>
+	Contact.findByIdAndDelete(contactId);
 
-export const addContact = async (name, email, phone) => {};
+export const addContact = async (data) => Contact.create(data);
 
-export const updateContactById = async (id, data) => {};
+export const updateContactById = (id, data) =>
+	Contact.findByIdAndUpdate(id, data);
